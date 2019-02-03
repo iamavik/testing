90a7ea6b44bf16d5fdb85e476145b3df1b201263
avik <avik@LAPTOP-2DTL1K74>
2019-02-03 17:10:58 +0530

    test commit

---
+++ setup.py
@@ -0,0 +1,7 @@
+'''
+Created on 29-Jan-2019
+
+@author: avik
+'''
+from setuptools import setup
+setup(name="testing",version="0.1",description="Basic system information for COMP30380",url="",author="Avik Saha",author_email="avik.saha@ucdconnect.ie",licence="GPL3",packages=['testing'],entry_points={'console_scripts':['comp30830_testing=testing.main:main']})
---
+++ testing/main.py
@@ -0,0 +1,11 @@
+'''
+Created on 29-Jan-2019
+
+@author: avik
+'''
+import platform
+def main():
+    print(platform.platform())
+    return 
+if __name__=='__main__':
+    main()
